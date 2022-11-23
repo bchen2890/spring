310e7419b0c2b3f5c162a1a689f1839c4090972c
bchen2890 <99276640+bchen2890@users.noreply.github.com>
2022-11-23 21:03:35 +0100

    GET Profile (@RequestParam + thymeleaf)

--- spring-tutorial/src/main/java/com/bchen/tutorial/spring/controller/MainController.java
+++ spring-tutorial/src/main/java/com/bchen/tutorial/spring/controller/MainController.java
@@ -1,14 +1,24 @@
 package com.bchen.tutorial.spring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
     @RequestMapping(value={"/index", "/", "/home"}, method = RequestMethod.GET)
     public String index() {
-
         return "index";
     }
+
+    @GetMapping("profile")
+    public String profile(@RequestParam String name, @RequestParam Integer age, Model model){
+        model.addAttribute("name", name);
+        model.addAttribute("age", age);
+        return "profile";
+    }
+
 }
