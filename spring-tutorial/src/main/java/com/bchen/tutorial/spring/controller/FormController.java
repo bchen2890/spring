c3c9eea568185c1309b08fca7a84b9617392c427
bchen2890 <99276640+bchen2890@users.noreply.github.com>
2022-11-30 10:56:46 +0100

    Form Validation
    @Valid, @ModelAttribute, @NotEmpty, th:object, th:field, th:errors

--- spring-tutorial/src/main/java/com/bchen/tutorial/spring/controller/FormController.java
+++ spring-tutorial/src/main/java/com/bchen/tutorial/spring/controller/FormController.java
@@ -1,10 +1,17 @@
 package com.bchen.tutorial.spring.controller;
 
+import com.bchen.tutorial.spring.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class FormController {
@@ -20,5 +27,51 @@ public class FormController {
         model.addAttribute("password", password);
         model.addAttribute("email", email);
         return "logged";
+    }
+
+    @GetMapping("/user-form")
+    public String getUserForm ( Model model){
+        User user = new User();
+        model.addAttribute("user", user);
+        return "userForm";
+    }
+
+    @PostMapping("/user-form")
+    public String userForm (@Valid @ModelAttribute("user") User user, BindingResult result, Model model){
+        if(result.hasErrors()) {
+            Map<String, String> errors = new HashMap<>();
+            result.getFieldErrors().forEach(err -> {
+                String field = err.getField();
+                errors.put(field, "The field " + field + " " + err.getDefaultMessage());
+            });
+            model.addAttribute("errors", errors);
+            return "userForm";
+        }
+        model.addAttribute("user", user);
+
+        return "userLogged";
+    }
+
+    @GetMapping("/auto-user-form")
+    public String getAutoUserForm ( Model model){
+        User user = new User();
+        model.addAttribute("user", user);
+        return "autoUserForm";
+    }
+
+    @PostMapping("/auto-user-form")
+    public String autoUserForm (@Valid @ModelAttribute("user") User user, BindingResult result, Model model){
+        if(result.hasErrors()) {
+            Map<String, String> errors = new HashMap<>();
+            result.getFieldErrors().forEach(err -> {
+                String field = err.getField();
+                errors.put(field, "The field " + field + " " + err.getDefaultMessage());
+            });
+            model.addAttribute("errors", errors);
+            return "autoUserForm";
+        }
+        model.addAttribute("user", user);
+
+        return "userLogged";
     }
 }
--- spring-tutorial/src/main/java/com/bchen/tutorial/spring/model/User.java
+++ spring-tutorial/src/main/java/com/bchen/tutorial/spring/model/User.java
@@ -1,9 +1,16 @@
 package com.bchen.tutorial.spring.model;
 
+import javax.validation.constraints.NotEmpty;
+
 public class User {
+    @NotEmpty
+    private String username;
     private String name;
     private Integer age;
-
+    @NotEmpty
+    private String email;
+    @NotEmpty
+    private String password;
     public User(){
 
     }
@@ -27,5 +34,29 @@ public class User {
 
     public void setAge(Integer age) {
         this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
