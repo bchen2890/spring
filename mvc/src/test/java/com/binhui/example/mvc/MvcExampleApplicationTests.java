32b7d96cf5bda0fd7a71c5f9e5c97cf747c71cce
bchen2890 <99276640+bchen2890@users.noreply.github.com>
2022-12-12 19:10:25 +0100

    Init MVC Project

---
+++ mvc/src/test/java/com/binhui/example/mvc/MvcExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binhui.example.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
